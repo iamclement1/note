a00f28b92a244a8b50af69336bc9f6ea84637232
iamclement1 <blessedmarcel1@gmail.com>
2021-10-23 18:51:19 +0100

    first commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListItem = () => {
+    return (
+        <div className="notes-list-item">
+            <h3></h3>
+            
+        </div>
+    )
+}
+
+export default ListItem
